dfcbbabd21b176b91623150ad9e33c4a1d2e9178
Аргунов Федор <fargunov@internet.ru>
2024-05-25 16:52:27 +0900

    Добавить логику для 3 клиента

--- MessageService.Client3/Program.cs
+++ MessageService.Client3/Program.cs
@@ -1,31 +1,16 @@
-public class Visitor
+using MessageService.Connector;
+using Refit;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://localhost:5050");
+var serverUrl = "http://localhost:5000";
+var refitClient = RestService.For<IMessageRestClient>(serverUrl);
+var app = builder.Build();
+app.MapGet("/getMessages", async () =>
 {
-    // static Semaphore _semaphore = new Semaphore(3, 3);
-    Thread myThread;
-    // int _count = 1; //счётчик посещений. Один раз сходить в клуб - достаточно
-    public Visitor(int i)
-    {
-        myThread = new Thread(Fun);
-        myThread.Name = $"Посетитель #{i}";
-        myThread.Start();
-    }
-    public void Fun()
-    {
-        // _semaphore.WaitOne();
-        Console.WriteLine($"{Thread.CurrentThread.Name} входит в клуб");
-        Console.WriteLine($"{Thread.CurrentThread.Name} веселится");
-        Thread.Sleep(1000); //безудержное веселье
-        Console.WriteLine($"{Thread.CurrentThread.Name} покидает клуб");
-       //  _semaphore.Release();
-    }
-}
-internal class Program
-{
-    static void Main(string[] args)
-    {
-        for (int i = 1; i < 7; i++)
-        {
-            Visitor visitor = new Visitor(i);
-        }
-    }
-}
+    var result = await refitClient.GetPushSoundsAsync(DateTime.UtcNow.AddMinutes(-10), DateTime.UtcNow);
+    return Results.Json(result);
+});
+
+
+await app.RunAsync();
