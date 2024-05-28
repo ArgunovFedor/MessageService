12fe8de6eb44add4bab5cc5c94e8868b40e741f9
Аргунов Федор <fargunov@internet.ru>
2024-05-28 13:24:03 +0900

    Обновить docker-compose файлы

--- MessageService.Client2/Program.cs
+++ MessageService.Client2/Program.cs
@@ -3,9 +3,12 @@ using System.Net.WebSockets;
 using System.Text;
 using MessageService.Abstractions.Messages;
 using MessageService.Client2.Infrastructure;
+using Serilog;
 using Newtonsoft.Json;
 
-// второй клиент при считывает по веб-сокету поток сообщений от сервера и отображает их в порядке прихода с сервера (с отображением метки времени и порядкового номера)
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseKestrel(options => options.AllowSynchronousIO = true)
@@ -13,40 +16,54 @@ builder.WebHost.UseKestrel(options => options.AllowSynchronousIO = true)
         {
             configurationBuilder.Configure(context, args);
         }
-    );
+    )
+    .ConfigureLogging(logging =>
+    {
+        logging.AddSerilog();
+    });
 
 var app = builder.Build();
 app.UseWebSockets();
 var connections = new List<WebSocket>();
+
 
 app.Map("/ws", async context =>
 {
     if (context.WebSockets.IsWebSocketRequest)
     {
         using var ws = await context.WebSockets.AcceptWebSocketAsync();
-        var curName = "test";
         connections.Add(ws);
-        await Broadcast($"{connections.Count} users connected");
-        await ReceiveMessage(ws,
-            async (result, buffer) =>
+        try
+        {
+            while (ws.State == WebSocketState.Open)
             {
-                if (result.MessageType == WebSocketMessageType.Text)
-                {
-                    var data = Encoding.UTF8.GetString(buffer, 0, result.Count);
-                    // Десериализация JSON
-                    var message = JsonConvert.DeserializeObject<MessageModel>(data);
-                    Console.WriteLine($"{DateTime.UtcNow} - {message.Text} {message.Number}");
-                    await Broadcast(curName + ": " + message);
-                }
-                else if (result.MessageType == WebSocketMessageType.Close || ws.State == WebSocketState.Aborted)
-                {
-                    connections.Remove(ws);
-                    await Broadcast($"{curName} left the room");
-                    await Broadcast($"{connections.Count} users connected");
-                    await ws.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription,
-                        CancellationToken.None);
-                }
-            });
+                await ReceiveMessage(ws,
+                    async (result, buffer) =>
+                    {
+                        if (result.MessageType == WebSocketMessageType.Text)
+                        {
+                            var data = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                            // Десериализация JSON
+                            var message = JsonConvert.DeserializeObject<MessageModel>(data);
+                            if (message != null)
+                            {
+                                Log.Information($"{DateTime.UtcNow} - {message.Text} {message.Number}");
+                                Console.WriteLine($"{DateTime.UtcNow} - {message.Text} {message.Number}");
+                            }
+                        }
+                        else if (result.MessageType == WebSocketMessageType.Close || ws.State == WebSocketState.Aborted)
+                        {
+                            await ws.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription,
+                                CancellationToken.None);
+                            connections.Remove(ws);
+                        }
+                    });
+            }
+        }
+        catch (WebSocketException ex) when (ex.WebSocketErrorCode == WebSocketError.ConnectionClosedPrematurely)
+        {
+            Console.WriteLine("WebSocket connection closed prematurely.");
+        }
     }
     else
     {
@@ -63,18 +80,4 @@ async Task ReceiveMessage(WebSocket socket, Action<WebSocketReceiveResult, byte[
         handleMessage(result, buffer);
     }
 }
-
-async Task Broadcast(string message)
-{
-    var bytes = Encoding.UTF8.GetBytes(message);
-    foreach (var socket in connections)
-    {
-        if (socket.State == WebSocketState.Open)
-        {
-            var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
-            await socket.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
-        }
-    }
-}
-
 await app.RunAsync();
